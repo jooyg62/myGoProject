314beb949fa53709b99454e45a84ebe2db29bbf7
jooyg62 <jooyg62@naver.com>
2023-07-29 17:51:23 +0900

    github upload

---
+++ package/keyword.go
@@ -0,0 +1,25 @@
+package keyboard
+
+import (
+        "log"
+        "bufio"
+        "os"
+        "strconv"
+        "strings"
+)
+
+func GetFloat() (float64, error) {
+        reader := bufio.NewReader(os.Stdin)
+        input, err := reader.ReadString('\n')
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        input = strings.TrimSpace(input)
+        number, err := strconv.ParseFloat(input, 64)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        return number, nil
+}
