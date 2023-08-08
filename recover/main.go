fb6ddd4527c0ed37ad6ea5a26a969d61aa372b34
jooyg62 <jooyg62@naver.com>
2023-08-08 22:39:26 +0900

    panic, defer, recover 으로 에러 처리

--- recover/main.go
+++ recover/main.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func calmDown() {
 	p := recover()
+	if p == nil {
+		return
+	}
 	err, ok := p.(error)
 	if ok {
 		fmt.Println(err.Error())
+	} else {
+		panic(p)
 	}
 	
 }
 
 func freakOut() {
 	defer calmDown()
+	panic("string panic")
 	err := fmt.Errorf("there's an error")
 	panic(err)
 }
